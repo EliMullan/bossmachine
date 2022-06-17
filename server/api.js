2ee2859e4812a054a5133de0d9daebf5d1663bcf
Eli Mullan <EliMullan21@gmail.com>
2022-06-17 08:39:59 +0100

    first commit

---
+++ server/api.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const apiRouter = express.Router();
+const {createMeeting,
+    getAllFromDatabase,
+    getFromDatabaseById,
+    addToDatabase,
+    updateInstanceInDatabase,
+    deleteFromDatabasebyId,
+    deleteAllFromDatabase} = require('./db'); 
+    
+
+
+module.exports = apiRouter;
