4d7561129a1db5b26652735d5e66bbf5b27e890a
Eli Mullan <EliMullan21@gmail.com>
2022-06-17 08:49:30 +0100

    boilerplate for api router

--- server/api.js
+++ server/api.js
@@ -7,7 +7,31 @@ const {createMeeting,
     updateInstanceInDatabase,
     deleteFromDatabasebyId,
     deleteAllFromDatabase} = require('./db'); 
-    
 
+//router for minions
+apiRouter
+    .get('/minions', (req, res) => {})
+    .post('/minions', (req, res) => {});
+
+apiRouter
+    .get('/minions/:minionId', (req, res) => {})
+    .put('/minions/:minionId', (req, res) => {})
+    .delete('/minions/:minionId', (req, res) => {});
+
+
+//router for ideas
+apiRouter
+    .get('/ideas', (req, res) => {})
+    .post('/ideas', (req, res) => {});
+
+apiRouter
+    .get('/ideas/:ideaId', (req, res) => {})
+    .put('/ideas/:ideaId', (req, res) => {})
+    .delete('/ideas/:ideaId', (req, res) => {});
+
+//router for meetings
+apiRouter
+    .get('/meetings', (req, res) => {})
+    .post('/meetings', (req, res) => {});
 
 module.exports = apiRouter;
