9afa36af30582ceab1e0b8f8095d62e86780d191
Eli Mullan <EliMullan21@gmail.com>
2022-06-18 18:20:21 +0100

    update minions and ideas

--- server/api.js
+++ server/api.js
@@ -7,27 +7,109 @@ const {createMeeting,
     updateInstanceInDatabase,
     deleteFromDatabasebyId,
     deleteAllFromDatabase} = require('./db'); 
+    const {checkMillionDollarIdea} = require('./checkMillionDollarIdea');
 
 //router for minions
+apiRouter.param('minionId', (req, res, next, id) => {
+    let minionId = id;
+    let minion = getFromDatabaseById('minions', minionId);
+    if (minion) {
+        req.minion = minion;
+        next();
+    } else {
+        res.status(404).send(`Minion not found`);
+    }
+})
 apiRouter
-    .get('/minions', (req, res) => {})
-    .post('/minions', (req, res) => {});
+    .get('/minions', (req, res) => {
+        res.send(getAllFromDatabase('minions'));
+    })
+    .post('/minions', (req, res) => {
+        let newMinion = {
+            name: req.body.name,
+            title: req.body.title,
+            weaknesses: req.body.weaknesses,
+            salary: Number(req.body.salary)
+        }
+        addToDatabase('minions', newMinion);
+        res.send(newMinion);
+    });
 
 apiRouter
-    .get('/minions/:minionId', (req, res) => {})
-    .put('/minions/:minionId', (req, res) => {})
-    .delete('/minions/:minionId', (req, res) => {});
+    .get('/minions/:minionId', (req, res) => {
+        let minionId = req.minion.id;
+        res.send(getFromDatabaseById('minions', minionId));
+    })
+    .put('/minions/:minionId', (req, res) => {
+       let updatedMinionId = req.minion.id;
+       let updatedMinion = {
+         id:updatedMinionId,
+         name: req.body.name,
+         title: req.body.title,
+         weaknesses: req.body.weaknesses,
+         salary: Number(req.body.salary)
+       }
+       updateInstanceInDatabase('minions', updatedMinion );
+       res.send(updatedMinion);
+    })
+    .delete('/minions/:minionId', (req, res) => {
+        let minionId = req.minion.id;
+        deleteFromDatabasebyId('minions', minionId);
+        res.status(202).send(`minion deleted`);
+    });
 
 
 //router for ideas
-apiRouter
-    .get('/ideas', (req, res) => {})
-    .post('/ideas', (req, res) => {});
+
+apiRouter.param('ideaId', (req, res, next, id) => {
+    let ideaId = id;
+    let idea = getFromDatabaseById('ideas', ideaId);
+    if (idea) {
+        req.idea = idea;
+        next();
+    } else {
+        res.status(404).send(`Idea not found`);
+    }
+});
+
 
 apiRouter
-    .get('/ideas/:ideaId', (req, res) => {})
-    .put('/ideas/:ideaId', (req, res) => {})
-    .delete('/ideas/:ideaId', (req, res) => {});
+    .get('/ideas', (req, res) => {
+        res.send(getAllFromDatabase('ideas'));
+    })
+    .post('/ideas', (req, res) => {
+        let newIdea = {
+            name: req.body.name,
+            description: req.body.description,
+            numWeeks: Number(req.body.numWeeks),
+            weeklyRevenue: Number(req.body.weeklyRevenue)
+        }
+        addToDatabase('ideas', newIdea);
+        res.send(newIdea);
+    });
+
+apiRouter
+    .get('/ideas/:ideaId', (req, res) => {
+        let ideaId = req.idea.id;
+        res.send(getFromDatabaseById('ideas', ideaId));
+    })
+    .put('/ideas/:ideaId', (req, res) => {
+        let updatedIdeaId = req.idea.id;
+        let updatedIdea = {
+          id:updatedIdeaId,
+          name: req.body.name,
+          description: req.body.description,
+          numWeeks: Number(req.body.numWeeks),
+          weeklyRevenue: Number(req.body.weeklyRevenue)
+        }
+        updateInstanceInDatabase('ideas', updatedIdea );
+        res.send(updatedIdea);
+    })
+    .delete('/ideas/:ideaId', (req, res) => {
+        let ideaId = req.idea.id;
+        deleteFromDatabasebyId('ideas', ideaId);
+        res.status(202).send(`idea deleted`);
+    });
 
 //router for meetings
 apiRouter
--- server/checkMillionDollarIdea.js
+++ server/checkMillionDollarIdea.js
@@ -1,4 +1,7 @@
-const checkMillionDollarIdea = () => {};
+const checkMillionDollarIdea = (numWeeks, weeklyRevenue) => {
+    let totalValue = numWeeks * weeklyRevenue; 
+    return totalValue;
+};
 
 // Leave this exports assignment so that the function can be used elsewhere
 module.exports = checkMillionDollarIdea;
